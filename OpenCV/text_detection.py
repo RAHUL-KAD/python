e48cb59ced33cd69730e6fa272b238d4f22d4d97
Rahul Kadam <kadamrahulj0909@gmail.com>
2020-11-25 15:48:28 +0530

    Update text_detection.py

--- OpenCV/text_detection.py
+++ OpenCV/text_detection.py
@@ -28,18 +28,18 @@ for b in boxes.splitlines():
 
 ## Detecting the Words:
 
-# Defining the locaion of the character
+# Defining the locaion of the word
 hImg, wImg, _ = rgb.shape
 boxes = pytesseract.image_to_data(rgb)
 
-# Drawing a red boxes around character and showing a character
-# for x, b in enumerate(boxes.splitlines()):
-    # if x != 0:
-        # b = b.split()
-        # if len(b) == 12:  
-            # x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
-            # cv.rectangle(rgb, (x, y), (w+x, y+h), (0, 0, 255), 3)
-            # cv.putText(rgb, b[11], (x-10, y), cv.FONT_HERSHEY_COMPLEX_SMALL, 5, (0, 255, 0), 2)
+# Drawing a red boxes around word and showing a words
+for x, b in enumerate(boxes.splitlines()):
+    if x != 0:
+        b = b.split()
+        if len(b) == 12:  
+            x, y, w, h = int(b[6]), int(b[7]), int(b[8]), int(b[9])
+            cv.rectangle(rgb, (x, y), (w+x, y+h), (0, 0, 255), 3)
+            cv.putText(rgb, b[11], (x-10, y), cv.FONT_HERSHEY_COMPLEX_SMALL, 5, (0, 255, 0), 2)
 
 cv.imshow('RGB image', rgb)
 cv.waitKey(0)
